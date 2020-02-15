754fc55b0ce642aafeb756867d68ed0450185d54
Ha Truong <hatruong@Has-MacBook.local>
2020-02-15 11:30:25 -0600

    Modify new datasets to expand dataframe

--- heartDiseases_explore.py
+++ heartDiseases_explore.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 
-data = np.genfromtxt('cleveland.data')
+data = np.genfromtxt('modified-data/cleveland.data')
+data2 = np.genfromtxt('modified-data/hungarian.data')
+data3 = np.genfromtxt('modified-data/switzerland.data')
+data4 = np.genfromtxt('modified-data/long-beach-va.data')
+data = np.concatenate((data,data2,data3,data4), axis = 0)
 needed = [3,4,9,10,12,16,19,32,38,40,41,44,51,58]
 index, deleted = 0, 0
 for col in range(1,77):
@@ -12,5 +16,6 @@ for col in range(1,77):
 		index += 1
 columns = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal", "num"]
 df = pd.DataFrame(data=data, columns=columns)
+#print(df)
 print("Mean values: ")
 print(np.mean(df, 0))
