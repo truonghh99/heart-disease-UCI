bd2c7f977beab194f7fcfb8599ae591270bfd12f
truonghh99 <truonghh@beloit.edu>
2020-03-02 17:40:39 -0600

    Add decision tree visualization

--- decision_tree.py
+++ decision_tree.py
@@ -6,7 +6,10 @@ from sklearn import metrics
 from sklearn.metrics import confusion_matrix 
 from sklearn.metrics import accuracy_score 
 from sklearn.metrics import classification_report    
-
+from sklearn import tree
+from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+import pydotplus
 
 # import all data
 data = np.genfromtxt('modified-data/cleveland.data')
@@ -51,3 +54,12 @@ clf = clf.fit(x_train,y_train)
 y_pred = clf.predict(x_test)
 
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+# visualize decision tree
+dot_data = StringIO()
+tree.export_graphviz(clf, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True,feature_names = feature_cols,class_names=['0','1','2','3','4'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png('heart_diseases.png')
+Image(graph.create_png())
