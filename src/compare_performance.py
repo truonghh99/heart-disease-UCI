e824312dfa7563c21fa5708adac05e6a4e1b9708
truonghh99 <truonghh@beloit.edu>
2020-03-25 21:40:04 -0700

    SVM performance

--- src/compare_performance.py
+++ src/compare_performance.py
@@ -9,6 +9,40 @@ from sklearn.metrics import classification_report
 from sklearn import tree
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn import svm
+from sklearn.metrics import plot_roc_curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+def plot_multiclass_roc(clf, X_test, y_test, n_classes, figsize):
+    y_score = clf.decision_function(X_test)
+
+    # structures
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    # calculate dummies once
+    y_test_dummies = pd.get_dummies(y_test, drop_first=False).values
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test_dummies[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # roc for each class
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot([0, 1], [0, 1], 'k--')
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('Heart Disease SVM Model performance')
+    for i in range(n_classes):
+        ax.plot(fpr[i], tpr[i], label='ROC curve (area = %0.2f) for label %i' % (roc_auc[i], i))
+    ax.legend(loc="best")
+    ax.grid(alpha=.4)
+    sns.despine()
+    plt.show()
 
 # import all data
 data = np.genfromtxt('../modified-data/cleveland.data')
@@ -51,7 +85,7 @@ y_test = testing[57] #target
 # classification object
 clf_1 = DecisionTreeClassifier()
 
-clf_1= clf_1.fit(x_train, y_train)
+clf_1 = clf_1.fit(x_train, y_train)
 y_pred_decision_tree = clf_1.predict(x_test)
 print("Accuracy using decision tree:", metrics.accuracy_score(y_test, y_pred_decision_tree))
 print("Confusion matrix: ")
@@ -64,3 +98,5 @@ y_pred_svm = clf_2.predict(x_test)
 print("Accuracy using svm:", metrics.accuracy_score(y_test, y_pred_svm))
 print("Confusion matrix: ")
 print(confusion_matrix(y_test, y_pred_svm))
+
+plot_multiclass_roc(clf_2, x_test, y_test, n_classes=5, figsize=(16, 10))
